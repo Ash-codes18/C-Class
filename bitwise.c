f5309798ec5a39ad73fed74f6de1b18897f857cb
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-01-31 20:00:05 +0530

    last day of jan

---
+++ bitwise.c
@@ -0,0 +1,26 @@
+//The following are the bitwise operatorrs in c :
+
+/*
+1. ) AND
+2. ) OR
+3. ) XOR
+4. ) Right shift
+5. ) Left shift
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int a=0;
+    int b=1;
+    printf("%d\n",23|56);
+    printf("%d\n",23&56);
+    printf("%d\n",0^0);
+    printf("%d\n",0^1);
+    printf("%d\n",1^0);
+    printf("%d\n",1^1);
+    printf("%d\n",5<<2);
+    return 0;
+}
+
