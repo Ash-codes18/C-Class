e57d7b4b302614f08d4706526e1e44e66ec239fe
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-05-02 11:16:55 +0530

    -Added Week 11 Answer 2 for Hackerrank

--- Hacker Rank Problems/Week 10/A2.c
+++ Hacker Rank Problems/Week 10/A2.c
@@ -27,27 +27,3 @@ int main() {
 
     return 0;
 }
-int main() {
-    int n;
-    scanf("%d", &n);
-
-    // check if n is within the given constraints
-    if (n < 5 || n > 50) {
-        printf("Invalid input");
-        return 0;
-    }
-
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i] % 5 == 0) {
-            printf("%d\n", arr[i]);
-        }
-    }
-    printf(" ");
-
-    return 0;
-}
